a77fa1b1c0f6cc337ea690868cb0d4560b7bda07
Mika <ext.mbenarab@librasoft.fr>
2024-01-10 19:11:53 +0100

    suite invoice form

--- my-dashboard/app/invoice/new/invoiceForm.tsx
+++ my-dashboard/app/invoice/new/invoiceForm.tsx
@@ -1,24 +1,32 @@
 "use client";
 
+import { Typography } from "@/components/ui/Typography";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
 import { Label } from "@/components/ui/label";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Textarea } from "@/components/ui/textarea";
-import { ServiceTypes } from "@/src/enums";
+import { cn } from "@/lib/utils";
 import { Customer } from "@prisma/client";
-import { SelectGroup } from "@radix-ui/react-select";
+import { Check, ChevronsUpDown } from "lucide-react";
 import { ChangeEvent, useState } from "react";
-import { Invoice, InvoiceLineSchema } from "../invoiceSchema";
+import { Invoice } from "../invoiceSchema";
 import { InvoiceLineForm } from "./invoiceLineForm";
+import { CustomerWithAddressAndContact } from "../page";
 
 type Props = {
-  customers: Customer[];
+  customers: CustomerWithAddressAndContact[];
 };
 function initInvoice() {
   const invoice: Invoice = {
@@ -48,6 +56,10 @@ function initInvoice() {
 }
 export const InvoiceForm = ({ customers }: Props) => {
   const [invoice, setInvoice] = useState<Invoice>(() => initInvoice());
+  const [selectedCustomer, setSelectedCustomer] =
+    useState<CustomerWithAddressAndContact | null>(null);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
 
   const onChangeLine = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
@@ -88,21 +100,82 @@ export const InvoiceForm = ({ customers }: Props) => {
           <CardTitle>Nouvelle Facture</CardTitle>
         </CardHeader>
         <CardContent>
-          <Label>Destinataire</Label>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Sélectionner destinataire" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {customers.map((c) => (
-                  <SelectItem key={c.id} value={c.businessName}>
-                    {c.businessName}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          <div className="w-1/2">
+            <Typography variant={"h3"}>Destinataire</Typography>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="min-w-[200px] justify-between"
+                >
+                  {value != "" ? value : "Sélectionner un destinataire..."}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput
+                    placeholder="Search framework..."
+                    className="h-9"
+                  />
+                  <CommandEmpty>Aucun client trouvé</CommandEmpty>
+                  <CommandGroup>
+                    {customers.map((customer) => (
+                      <CommandItem
+                        key={customer.id}
+                        value={customer.businessName}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue);
+                          setOpen(false);
+                          setSelectedCustomer(customer);
+                        }}
+                      >
+                        {customer.businessName}
+                        <Check
+                          className={cn(
+                            "ml-auto h-4 w-4",
+                            value === customer.businessName
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </Command>
+              </PopoverContent>
+            </Popover>
+            <div className="flex gap-5">
+              <div>
+                <p className="h-6 min-h-6">Société:</p>
+                <p className="h-6 min-h-6">Nom:</p>
+                <p className="h-6 min-h-6">Adresse</p>
+                <p className="h-6 min-h-6">Pays:</p>
+                <p className="h-6 min-h-6">Numéro d entreprise:</p>
+                <p className="h-6 min-h-6">Numéro de TVA:</p>
+                <p className="h-6 min-h-6">Adresse email:</p>
+              </div>
+              <div className="">
+                <p className="h-6 min-h-6">{selectedCustomer?.businessName}</p>
+                <p className="h-6 min-h-6">
+                  {selectedCustomer?.contact?.firstName}
+                </p>
+                <p className="h-6 min-h-6">
+                  {selectedCustomer?.address[0].addressName}
+                </p>
+                <p className="h-6 min-h-6">
+                  {selectedCustomer?.address[0].country}
+                </p>
+                <p className="h-6 min-h-6">{selectedCustomer?.siren}</p>
+                <p className="h-6 min-h-6">{selectedCustomer?.siren}</p>
+                <p className="h-6 min-h-6">
+                  {selectedCustomer?.contact?.email}
+                </p>
+              </div>
+            </div>
+          </div>
           <Label>Articles</Label>
           {invoice.lines.map((line) => (
             <InvoiceLineForm
--- my-dashboard/app/invoice/page.tsx
+++ my-dashboard/app/invoice/page.tsx
@@ -1,8 +1,16 @@
 import { prisma } from "@/prisma/client";
 import { InvoiceForm } from "./new/invoiceForm";
+import { Prisma } from "@prisma/client";
+
+export type CustomerWithAddressAndContact = Prisma.CustomerGetPayload<{
+  include: { contact: true; address: true };
+}>;
 
 export default async function InvoicePage() {
-  const customers = await prisma.customer.findMany();
+  const customers = await prisma.customer.findMany({
+    include: { contact: true, address: true },
+  });
+
   return (
     <div>
       <InvoiceForm customers={customers} />
